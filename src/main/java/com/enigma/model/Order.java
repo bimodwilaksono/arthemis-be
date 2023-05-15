8a299d861bf91513e87cfa381bf2d6873269a471
bimodwilaksono <bimodwilaksono@email.com>
2023-05-15 11:10:14 +0700

    [ADD] Order Model and Repository

---
+++ src/main/java/com/enigma/model/Order.java
@@ -0,0 +1,39 @@
+package com.enigma.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order {
+
+    @Id
+    private Integer id;
+
+    private String status;
+
+    @Column(name = "checkin_date")
+    private LocalDate checkInDate;
+
+    @Column(name = "checkout_date")
+    private LocalDate checkOutDate;
+
+    //Many To Many with Customer
+    @ManyToOne
+    private User user;
+
+    //OneToOne with Campsite
+//    @OneToOne
+//    private Campsite campsite
+
+    //OneToOne with Payment
+//    @OneToOne
+//    private Payment payment
+
+
+}
